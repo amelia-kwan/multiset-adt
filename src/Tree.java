f016dc672f87c15e3dd52bf4764e7d55310d5d12
epbal <e.balaniuk@mail.utoronto.ca>
2023-09-18 14:38:25 -0400

    Tree initializer translated.

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Tree {
+    private Object root;
+    private List<Tree> subtrees;
+
+    public Tree(Object root, List<Tree> subtrees){
+        this.root = root;
+        if (subtrees == null) {
+            this.subtrees = new ArrayList<>();
+        }
+        else {
+            this.subtrees = new ArrayList<>(subtrees);
+        }
+    }
 }
+
+
